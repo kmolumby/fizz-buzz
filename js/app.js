49547465aa3bfaf0886c7ae091d5e15181e4254e
kmolumby <kmolumby@gmail.com>
2016-01-18 17:59:41 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function){
+	$("body")on.("click","button",function(event){
+
+	
+
+		for (var i=1; i<=100; i++) {
+
+		if (i%5 ===0 && i%3 ===0) {
+			console.log("FizzBuzz");
+		};
+
+		else if (i%3 ===0) {
+			console.log("Fizz");
+		;
+
+		else if (i%5 ===0) {
+			console.log("Buzz")
+		};
+
+
+
+		});
+
+	};
+
+};
